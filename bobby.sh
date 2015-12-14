04028725fb7da642d84b47927d7c31f09b2a8f9b
Rafael Torres <iterativo@users.noreply.github.com>
2015-12-14 10:44:12 -0600

    Hook notification

--- bobby.sh
+++ bobby.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 # TODO Description
 
+# URL txt check vars
 str="<span class=\"customer_commit_display\">In Stock</span>"
 url="http://www.apple.com/shop/product/G0RG1LL/A/refurbished-154-inch-macbook-pro-28ghz-quad-core-intel-i7-with-retina-display"
 
+# Notification vars
+title="MBP is available!"
+msg="Go to refurbished outlet"
+sound="default"
+
 if curl -s "$url" | grep -q -i "$str"; then
-	echo "found"
+	osascript -e 'display notification "'"$msg"'" with title "'"$title"'" sound name "'"$sound"'"'
 else
 	echo "not found"
 fi
