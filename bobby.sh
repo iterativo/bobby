3cc50ef87c43e7f6e4cf03c35677c562dc484dff
Rafael Torres <iterativo@users.noreply.github.com>
2015-12-15 14:52:17 -0600

    Add usage message

--- bobby.sh
+++ bobby.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 # TODO Description
 
-if [ $# -lt 2 ]; then
-	echo "Error: Invalid number of args"
+appname="$(basename "$0")"
+
+: ${title:="$appname"}
+: ${msg:="Found text in URL"}
+: ${sound:="Ping.aiff"}
+
+usage="
+Usage: ./$appname [-h] url txt [-t title] [-m msg] [-s sound]
+-- Program to check URL content for provided text (case insensitive) and generate Apple Script notification when found.
+
+where:
+  -h   show this help text
+  url  URL's whose content will be searched
+  txt  text expected to be found in URL
+  -t   title for Apple Script notification (default: $title)
+  -m   message to be shown in Apple Script notification (default: $msg)
+  -s   sound name for Apple Script notification (default: $sound)
+
+Example:
+  ./$appname http://www.amazon.com/products/1 \"in stock\""
+
+if [ $# -lt 2 ] || [ $1 -eq -h ]; then
+	echo "$usage"
 	exit
 fi
 
