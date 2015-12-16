cbf6979d3cdd4fb4d77d1879fd5a3f47bad8ebbc
Rafael Torres <iterativo@users.noreply.github.com>
2015-12-16 09:17:34 -0600

    Implement options parsing

--- bobby.sh
+++ bobby.sh
@@ -3,11 +3,15 @@
 
 appname="$(basename "$0")"
 
-### Options
+########################
+#        Options       #
+########################
+
 : ${title:="$appname"}
 : ${msg:="Found text in URL"}
 : ${sound:="Ping.aiff"}
 
+# Help text
 usage="
 Usage: ./$appname [-h] url txt [-t title] [-m msg] [-s sound]
 -- Program to check URL content for provided text (case insensitive) and notify user when found.
@@ -23,23 +27,68 @@ where:
 Example:
   ./$appname http://www.amazon.com/products/1 \"in stock\""
 
-### Args length requirements check
+########################
+#         Args         #
+########################
+
+# Check args were provided
 if [ $# -lt 2 ] || [ $1 == "-h" ]; then
 	echo "$usage"
 	exit
 fi
 
-### Args
 url=$1
 txt=$2
 
+# Check URL validity
 urlregex="(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]"
 if ! [[ $url =~ $urlregex ]]; then
-	echo "ERROR: Invalid 'url' arg - please enter a valid URL."
+	echo "ERROR: Invalid 'url' arg. Please enter a valid URL."
 	echo "For usage, run: ./$appname -h"
-	exit
+	exit 1
 fi
 
+# Parse options
+shift; shift
+while test $# -gt 0; do
+        case "$1" in
+                -t)
+                        shift
+                        if test $# -gt 0; then
+                                title=$1
+                        else
+                                echo "no title specified"
+                                exit 1
+                        fi
+                        shift
+                        ;;
+                -m)
+                        shift
+                        if test $# -gt 0; then
+                                msg=$1
+                        else
+                                echo "no msg specified"
+                                exit 1
+                        fi
+                        shift
+                        ;;
+                -s)
+                        shift
+                        if test $# -gt 0; then
+                                sound=$1
+                        else
+                                echo "no sound specified"
+                                exit 1
+                        fi
+                        shift
+                        ;;
+                *)
+                        break
+                        ;;
+        esac
+done
+
+# Execute URL content test
 if curl -s "$url" | grep -q -i "$txt"; then
 	osascript -e 'display notification "'"$msg"'" with title "'"$title"'" sound name "'"$sound"'"'
 else
